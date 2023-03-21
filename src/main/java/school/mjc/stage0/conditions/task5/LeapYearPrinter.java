6d6ac1e7b0d2b485449c909dc80bf3eeb5d95f48
aniar <aniar.kaldybaiuly@nu.edu.kz>
2023-03-21 22:05:17 +0600

    v1

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean res = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        int result = res == true ? 1 : 0;
+        switch (result) {
+            case 1:
+                System.out.println("leap");
+                break;
+            case 0:
+                System.out.println("not leap");
+                break;
+        }
     }
 }
